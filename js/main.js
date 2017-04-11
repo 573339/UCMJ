0c3988f52bfc244af52115c4727a67ffc32ce132
Jonathan Cronin <cronin_jonathan@bah.com>
2017-04-11 10:13:06 -0400

    progress and search updates

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ $.extend($.expr[":"], {
 
 var currentArticle,
 	currentScenario,
-	currentScenarioScreen;
+	currentScenarioScreen,
+	$isotope;
 
 var articles=[
 	{
@@ -62,6 +63,49 @@ var articles=[
 						"description": "Any interaction with property that is not your own could be misconstrued. Wrongfully taking, obtaining, or withholding, by any means, from the possession of the owner or of any other person any money, personal, property, or article of value of any kind is a punishable offense."
 					}
 				}
+			},
+			{
+				"id": 1,
+				"article": "921.ARTICLE 121",
+				"title": "Scenario 1",
+				"content":[
+					{
+						"image": "scenario-01.jpg",
+						"text": "Early in the morning, your fellow recruits prepare to move out on a busy training schedule."
+					},
+					{
+						"image": "scenario-02.jpg",
+						"text": "Later that day when no one is around, you notice something unusual."
+					},
+					{
+						"image": "scenario-03.jpg",
+						"text": "A couple of your fellow recruits have left their racks open."
+					},
+					{
+						"image": "scenario-04.jpg",
+						"text": "Upon closer inspection you clearly see a wallet inside."
+					}
+				],
+				"test":{
+					"prompt": "What do you do?",
+					"description": "Upon closer inspection, you clearly see a wallet inside",
+					"options":[
+						{
+							"text": "Close the locker"
+						},
+						{
+							"text": "Report the issue to a superior",
+							"correct": true
+						},
+						{
+							"text": "Take the unattended valuables"
+						}
+					],
+					"response":{
+						"title": "Report the issue to a superior",
+						"description": "Any interaction with property that is not your own could be misconstrued. Wrongfully taking, obtaining, or withholding, by any means, from the possession of the owner or of any other person any money, personal, property, or article of value of any kind is a punishable offense."
+					}
+				}
 			}
 		]
 	},
@@ -73,7 +117,51 @@ var articles=[
 		"image": "larceny.jpg",
 		"libraryImage": "absence-article.jpg",
 		"progress": "underway",
-		"scenarios":[]
+		"scenarios":[
+			{
+				"id": 0,
+				"article": "921.ARTICLE 121",
+				"title": "Scenario 1",
+				"content":[
+					{
+						"image": "scenario-01.jpg",
+						"text": "Early in the morning, your fellow recruits prepare to move out on a busy training schedule."
+					},
+					{
+						"image": "scenario-02.jpg",
+						"text": "Later that day when no one is around, you notice something unusual."
+					},
+					{
+						"image": "scenario-03.jpg",
+						"text": "A couple of your fellow recruits have left their racks open."
+					},
+					{
+						"image": "scenario-04.jpg",
+						"text": "Upon closer inspection you clearly see a wallet inside."
+					}
+				],
+				"test":{
+					"prompt": "What do you do?",
+					"description": "Upon closer inspection, you clearly see a wallet inside",
+					"options":[
+						{
+							"text": "Close the locker"
+						},
+						{
+							"text": "Report the issue to a superior",
+							"correct": true
+						},
+						{
+							"text": "Take the unattended valuables"
+						}
+					],
+					"response":{
+						"title": "Report the issue to a superior",
+						"description": "Any interaction with property that is not your own could be misconstrued. Wrongfully taking, obtaining, or withholding, by any means, from the possession of the owner or of any other person any money, personal, property, or article of value of any kind is a punishable offense."
+					}
+				}
+			}
+		]
 	},
 	{
 		"id": 2,
@@ -159,34 +247,73 @@ function changeTitle(newTitle){
 	});
 }
 
+function arrayScan(target, key){
+	var found=0;
+	$.each(target,function(){
+		if(this[key]){
+			found++;
+		}
+	});
+	return found;
+}
+
 $(document).ready(function(){
 
+	
+	if(localStorage.getItem('ucmjArticles')){
+		articles=JSON.parse(localStorage.getItem('ucmjArticles'));
+	}
 
+	
 	//populates article library
-	$.each(articles, function(){
-		var newArticle=$('<div class="col-md-4 tile-hover article-tile progress-'+this.progress+'">'+
-				'<a href="'+this.id+'">'+
-					'<div class="absence " style="background-image: url(img/'+this.libraryImage+'">'+
-						'<div class="tile-content">'+
-							'<span>'+this.article+'</span>'+
-							'<h3>'+this.title+'</h3>'+
-						'</div>'+
-						'<div class="status">'+
-							'<p>0 of '+this.scenarios.length+' Completed</p>'+
-						'</div>'+
-					'</div>'+
-				'</a>'+
-			'</div>');
+	function reloadLibrary(){
+		$('#articles-library').empty();
+		$.each(articles, function(){
+			
+			var progressString;
+			switch (arrayScan(this.scenarios,'complete')/this.scenarios.length){
+				case 1:
+					progressString='complete';
+					break;
+				case 0:
+					progressString='new';
+					break;
+				default:
+					progressString='underway';		
+			}
 
-		newArticle.appendTo('#articles-library');
-	});
+			var newArticle=$('<div class="col-md-4 tile-hover article-tile progress-'+progressString+'">'+
+					'<a href="'+this.id+'">'+
+						'<div class="absence " style="background-image: url(img/'+this.libraryImage+'">'+
+							'<div class="tile-content">'+
+								'<span>'+this.article+'</span>'+
+								'<h3>'+this.title+'</h3>'+
+							'</div>'+
+							'<div class="status">'+
+								'<p>'+arrayScan(this.scenarios,'complete')+' of '+this.scenarios.length+' Completed</p>'+
+							'</div>'+
+						'</div>'+
+					'</a>'+
+				'</div>');
 
-	//init isotope
-	var $isotope=$('#articles-library').isotope({
-		// options
-		itemSelector: '.article-tile',
-		layoutMode: 'fitRows'
-	});
+			newArticle.appendTo('#articles-library');
+		});
+
+		//init isotope
+		if($isotope){
+			$isotope.isotope('destroy');
+		}
+		else{
+			$isotope=$('#articles-library').isotope({
+				// options
+				itemSelector: '.article-tile',
+				layoutMode: 'fitRows'
+			});
+		}
+		
+
+	}
+	reloadLibrary();
 
 	//update filter function, search and progress
 	function updateFilter(){
@@ -242,15 +369,19 @@ $(document).ready(function(){
 		updateFilter();
 	});
 
-	//makes article links active
-	$('#articles-library a').click(function(){
-			
+	function reloadScenarios(){
 		//empty article scenarios bin, set current article
 		$('.article-scenarios').empty();
-		currentArticle=articles[$(this).attr('href')];
 
 		//populates article scenarios
 		$.each(currentArticle.scenarios, function(index){
+			var completeIcon='times-circle',
+			progressString='progress-new';
+
+			if(this.complete){
+				completeIcon='check-circle';
+				progressString='progress-complete';
+			}
 
 			var newScenario=$('<div class="col-md-3 tile-hover grid">'+
 				'<a href="'+this.id+'">'+
@@ -258,7 +389,7 @@ $(document).ready(function(){
 						'<div class="tile-content">'+
 							'<span>'+currentArticle.article+'</span>'+
 							'<h2>'+this.title+'</h2>'+
-							'<p><i class="fa fa-check-circle" aria-hidden="true"></i>'+(index+1)+' of '+currentArticle.scenarios.length+'</p>'+
+							'<p><i class="fa fa-'+completeIcon+'" aria-hidden="true"></i>'+(index+1)+' of '+currentArticle.scenarios.length+'</p>'+
 						'</div>'+
 						'<div class="status">'+
 							'<button type="button" class="btn btn-default redo">Start<i class="fa fa-angle-right" aria-hidden="true"></i></button>'+
@@ -287,7 +418,13 @@ $(document).ready(function(){
 		$('#article-article').text(currentArticle.article);
 		$('#article-title').text(currentArticle.title);
 		$('#article-description').text(currentArticle.description);
+	}
 
+	//makes article links active
+	$('#articles-library').on('click','a',function(){
+		
+		currentArticle=articles[$(this).attr('href')];
+		reloadScenarios();
 
 		$('#articles').fadeOut(function(){
 			$('.screen-article').fadeIn();
@@ -298,6 +435,7 @@ $(document).ready(function(){
 
 	//back to articles
 	$('#btn-articles-back').click(function(){
+		reloadLibrary();
 		$('.screen-article').fadeOut(function(){
 			$('.screen-articles').fadeIn();
 		});
@@ -332,6 +470,8 @@ $(document).ready(function(){
 			$('#testModal .modal-footer a').click(function(){
 				if($(this).hasClass('test-correct')){
 					$('#responseModal .modal-subtitle').removeClass('correct incorrect').addClass('correct').text('Correct');
+					currentScenario.complete=true;
+					localStorage.setItem('ucmjArticles',JSON.stringify(articles));
 				}
 				else{
 					$('#responseModal .modal-subtitle').removeClass('correct incorrect').addClass('incorrect').text('Incorrect');
@@ -361,6 +501,7 @@ $(document).ready(function(){
 	$('.btn-test-toarticle').click(function(){
 		$('#responseModal').modal('hide');
 		changeTitle('Primer');
+		reloadScenarios();
 		$('.screen-scenario').fadeOut(function(){
 			$('.screen-article').fadeIn();
 		});
